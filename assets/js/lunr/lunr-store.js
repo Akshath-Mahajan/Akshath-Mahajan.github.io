b7b40fd6540347361ea9fdf023cbd7e6e20ae8bd
Akshath-Mahajan <Akshath-Mahajan@users.noreply.github.com>
2025-04-07 17:15:55 +0000

    deploy: 1fc350496c1ff17346c1eb396e3dcf1663ed56df

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,127 @@
+var store = [{
+        "title": "Undergraduate AI Researcher",
+        "excerpt":"Achieved 29.89% decrease in reconstruction time for single image super-resolution and 8% better SSIM scores compared to state-of-the-art techniques Designed innovative neural architectures for hierarchical classification, improving prediction accuracy by 2% for finer and 15% for coarser levels Created a large satellite dataset for tropical cyclones and forecasted wind-speeds with...","categories": [],
+        "tags": [],
+        "url": "/experience/ai-researcher",
+        "teaser": null
+      },{
+        "title": "MLOps Intern",
+        "excerpt":"   Reviewed literature on 15+ techniques for apparel try-on and selected potential candidates for the given dataset   Deployed and integrated 3 deep learning models on AWS Lambda using Docker   Implemented data pipeline enhancements, improving the core product’s efficiency by 15%  ","categories": [],
+        "tags": [],
+        "url": "/experience/mlops-intern",
+        "teaser": null
+      },{
+        "title": "React Intern",
+        "excerpt":"   Implemented a customized admin interface in React with 5+ features   Defined real-time data pipelines using Firebase, creating 7+ endpoints   Increased user engagement by adding 4+ spring-based animations   Optimized site performance using SEO, reducing initial response time by over 80%   Here’s the Finplex Website  ","categories": [],
+        "tags": [],
+        "url": "/experience/react-intern",
+        "teaser": null
+      },{
+        "title": "Fullstack Developer",
+        "excerpt":"   Spearheaded the design and development of the official Tinkerer’s Lab website   Engineered backend logic to manage equipment rentals and inventory tracking   Completed development of 20+ features while rapidly responding to changing requirements  ","categories": [],
+        "tags": [],
+        "url": "/experience/fullstack-developer",
+        "teaser": null
+      },{
+        "title": "Software Engineer",
+        "excerpt":"Integrated 4+ data visualization graphs and bubble plots in React to analyze and present complex analytical data Developed 12+ features for tracking propaganda posts by monitoring bot-like activity, inauthentic sharing, post spread, and coordinated sharing Created interactive dashboards using React and D3.js to manipulate and explore latent propaganda information in...","categories": [],
+        "tags": [],
+        "url": "/experience/software-engineer",
+        "teaser": null
+      },{
+        "title": "K12 Machine Learning Instructor",
+        "excerpt":"   Conceptualized and developed the NYU K-12 Machine Learning Summer Program curriculum, integrating cutting-edge ML techniques with interactive, student-friendly content   Instructed 70+ high school students in machine learning fundamentals and advanced techniques   Led hands-on projects and coding exercises, guiding students through model creation and deployment   Program Website  ","categories": [],
+        "tags": [],
+        "url": "/experience/k12-ml-instructor",
+        "teaser": null
+      },{
+        "title": "Research Assistant",
+        "excerpt":"   Parallelized and accelerated RLWE algorithms using a vector processor unit   Designed 5+ kernels for Fully Homomorphic Encryption (FHE)   Implementing compiler optimizations to maximize resource utilization  ","categories": [],
+        "tags": [],
+        "url": "/experience/research-assistant",
+        "teaser": null
+      },{
+        "title": "E-Commerce Website",
+        "excerpt":"   Led the successful development of an E-Commerce website with 25+ features including secure payment gateways and order management systems   Led site deployment and incorporated independent servers, reducing hosting costs by 31%   Streamlined functionality using Redux for global state management, reducing code complexity by 40%  ","categories": ["Web Development"],
+        "tags": ["ecommerce","redux","deployment"],
+        "url": "/projects/ecommerce-website",
+        "teaser": null
+      },{
+        "title": "Official Website for DJ Unicode",
+        "excerpt":"   Led the re-development of DJ Unicode’s official website, accredited by DJSCE   Introduced a Graph Database system to enhance content management, increasing efficiency and organization of data by 50%   Improved site performance and reduced load time by 18% using optimal SEO techniques   Visit Project Site  ","categories": ["Web Development"],
+        "tags": ["website","leadership","SEO"],
+        "url": "/projects/dj-unicode-website",
+        "teaser": null
+      },{
+        "title": "Hierarchical Classification using Ranked Skip Connections",
+        "excerpt":"   Designed innovative neural architectures for hierarchical classification, improving prediction accuracy by 2% at finer levels and 15% at coarser levels compared to prior techniques   Reduced disparity between predictions across levels of bird taxonomy hierarchy by applying an ensemble learning approach, resulting in a 12% increase in overall classification accuracy  ","categories": ["Deep Learning","Computer Vision"],
+        "tags": ["hierarchical-classification","ensemble-learning","neural-networks"],
+        "url": "/projects/hierarchical-classification",
+        "teaser": null
+      },{
+        "title": "Prediction of Disease Predisposition by Analyzing Gene Reports",
+        "excerpt":"   Performed Knowledge Graph Completion based on gene report data   Used Graph Machine Learning to predict gene–disease and variant–disease associations   Implemented disease–drug associations to predict suitable preventive drugs  ","categories": ["NLP","Deep Learning"],
+        "tags": [],
+        "url": "/projects/gene-disease-prediction",
+        "teaser": null
+      },{
+        "title": "MIPS 5-Stage Pipeline Simulator with Tomasulo",
+        "excerpt":"Developed a cycle-accurate simulator for a 5-stage pipelined MIPS processor in C++, ensuring correct architectural state at every clock cycle Designed mechanisms to address control hazards using forwarding, stalling, and branch prediction Boosted efficiency with 2-level branch prediction, improving performance by 20% over baseline implementations Implemented Tomasulo’s algorithm with hazard...","categories": ["Computer Architecture","Software Development"],
+        "tags": ["mips","simulator","pipelining","tomasulo"],
+        "url": "/projects/mips-pipeline-simulator",
+        "teaser": null
+      },{
+        "title": "Two-Level Cache Simulator",
+        "excerpt":"   Designed a two-level exclusive cache simulator with configurable block size, associativity, and eviction policies   Implemented L1 (two-way) and L2 (four-way) caches with write-back and no-write-allocate policies   Optimized eviction and dirty bit handling to improve cache performance and reduce miss penalties by 5%+  ","categories": ["Computer Architecture","Software Development"],
+        "tags": [],
+        "url": "/projects/cache-simulator",
+        "teaser": null
+      },{
+        "title": "Low Resource Image Super Resolution",
+        "excerpt":"   Proposed pre-upsampling techniques to achieve 32.99 PSNR and 0.9051 SSIM with minimal training   Implemented and compared 6 unique techniques for image super resolution under constrained training budgets   Achieved 1.5× performance over baseline GANs using pre-upsampled UNet architectures  ","categories": ["Deep Learning","Computer Vision","GANs"],
+        "tags": ["super-resolution","GANs","low-resource"],
+        "url": "/projects/low-resource-super-resolution",
+        "teaser": null
+      },{
+        "title": "Vector Processor Simulator",
+        "excerpt":"Developed a cycle-accurate simulator for VMIPS-based vector processors with 6 pipelined functional units Implemented machine learning algorithms using the ISA, including dot products, matrix multiplications, and strided convolution layers Conducted design space exploration to optimize configurations, resulting in up to 35% faster execution across benchmarks Introduced novel architecture optimizations, improving...","categories": ["Computer Architecture","Software Development"],
+        "tags": ["vector-processing","simulator","pipelining","ml-workloads"],
+        "url": "/projects/vector-processor-simulator",
+        "teaser": null
+      },{
+        "title": "CUDA-Optimized Large Vision-Language Models",
+        "excerpt":"   Achieved 390× CPU speedup for Vision-Language Models (VLMs) with negligible loss in task-specific performance   Reduced model size by 95%+ using parallelization and network compression techniques   Achieved inference speed of 20–30 FPS on CPU  ","categories": ["High-Performance Computing"],
+        "tags": [],
+        "url": "/projects/cuda-vlm",
+        "teaser": null
+      },{
+        "title": "Neural Machine Translation from English to Marathi Using Various Techniques",
+        "excerpt":"Machine translation (MT) is a term used to describe computerized systems that generate translations from one linguistic communication to another, either with or without the need of humans. Text can be used to evaluate knowledge and converting that information to visuals can help in communication and information acquisition. There have...","categories": ["manuscripts"],
+        "tags": [],
+        "url": "/publication/2022-05-13-Neural-Machine-Translation-from-English-to-Marathi-Using-Various-Techniques",
+        "teaser": null
+      },{
+        "title": "CyINSAT: Cyclone Dataset from Indian National Satellite for Forecasting",
+        "excerpt":"Tropical cyclones in India are annual natural disasters that have taken a toll of 1 lakh casualties in total to date and established the North Indian Ocean (NIO) as the deadliest basin historically. Moreover, it causes property damage and can lead to soil erosion, having adverse effects wherever it makes...","categories": ["manuscripts"],
+        "tags": [],
+        "url": "/publication/2022-11-24-CyINSAT",
+        "teaser": null
+      },{
+        "title": "Forecasting North Indian Ocean Tropical Cyclone Intensity",
+        "excerpt":"India is prone to tropical cyclones annually, originating from the North Indian Ocean basin. Tropical cyclones are destructive and sudden natural occurrences that annually wreak havoc by taking a huge toll on human lives and property. This engenders a need for accurately forecasting the scale of such mass-destructive events, to...","categories": ["manuscripts"],
+        "tags": [],
+        "url": "/publication/2022-12-10-Forecasting-North-Indian-Ocean-Tropical-Cyclone-Intensity",
+        "teaser": null
+      },{
+        "title": "FASRGAN: Feature Attention Super Resolution Generative Adversarial Network",
+        "excerpt":"The advent of multimedia-based communication requires high-speed, memory-efficient data storage of high quality images. One method for reducing the dimensions of an image while preserving the finer texture characteristics is image super resolution. This paper proposes a new model to deal with the reconstruction time and artifacts that GAN-based deep...","categories": ["manuscripts"],
+        "tags": [],
+        "url": "/publication/2023-09-21-FASRGAN",
+        "teaser": null
+      },{
+        "title": "Deep Learning Techniques for Thought-Controlled IoT Devices",
+        "excerpt":"The human brain is made up of millions of neurons that are responsible for directing the activity of the human body in response to internal–external stimuli generated by motor and sensory inputs. Quotidian tasks rely on this basic functioning of our body, which could be simplified by harbouring the advancements...","categories": ["manuscripts"],
+        "tags": [],
+        "url": "/publication/2025-01-16-Deep-Learning-for-Thought-Controlled-IoT-Devices",
+        "teaser": null
+      }]
